92eb2d0556fde1a5fc61b317a02c6ba82093af71
Felix Lopez <Lfelixfermin@gmail.com>
2023-07-22 09:07:37 -0600

    UPDATE:FrmMostrarPlan cambio en el diseño del data

--- UNAN/Presentacion/MostrarPlan.Designer.cs
+++ UNAN/Presentacion/MostrarPlan.Designer.cs
@@ -28,27 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dtDetallePlan = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dtDetallePlan)).BeginInit();
             this.SuspendLayout();
             // 
             // dtDetallePlan
             // 
+            this.dtDetallePlan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtDetallePlan.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dtDetallePlan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtDetallePlan.DefaultCellStyle = dataGridViewCellStyle1;
             this.dtDetallePlan.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dtDetallePlan.Location = new System.Drawing.Point(0, 0);
+            this.dtDetallePlan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtDetallePlan.Name = "dtDetallePlan";
             this.dtDetallePlan.RowHeadersWidth = 82;
             this.dtDetallePlan.RowTemplate.Height = 33;
-            this.dtDetallePlan.Size = new System.Drawing.Size(2328, 803);
+            this.dtDetallePlan.Size = new System.Drawing.Size(685, 389);
             this.dtDetallePlan.TabIndex = 0;
             // 
             // MostrarPlan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2328, 803);
+            this.ClientSize = new System.Drawing.Size(685, 389);
             this.Controls.Add(this.dtDetallePlan);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MostrarPlan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Detalle Plan Didactico";
