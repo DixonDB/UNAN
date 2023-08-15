71bc94f8f5460be2df42917ab5e6f8efe9fed38f
Felix Lopez <Lfelixfermin@gmail.com>
2023-08-15 09:37:33 -0600

    ADD:Documentacion de DAvanceProgramatico y DCarreras

--- UNAN/Datos/DAvanceProgramatico.cs
+++ UNAN/Datos/DAvanceProgramatico.cs
@@ -1,31 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using UNAN.Logica;
-using System.Security.RightsManagement;
-using System.Data.SqlTypes;
 
 namespace UNAN.Datos
 {
     public class DAvanceProgramatico
     {
+        /// <summary>
+        /// Muestra el ultimo tema por profesor desde la base de datos en un ComboBox que se autocompleta con dicha información.
+        /// </summary>
+        /// <param name="txt">El combobox en el cual se mostrará el último tema</param>
+        /// <param name="idProfesor">El idprofesor para filtrar el tema</param>
+        /// <param name="idcarrera">El idcarrera para filtrar el tema</param>
+        /// <param name="idasignatura">El idasignatura para filtrar el tema</param>
         public void MostrarUltimoTema(TextBox txt, int idProfesor, int idcarrera, int idasignatura)
         {
             try
             {
-
+                //Abrir la conexión a la base de datos
                 Conexion.abrir();
+
+                //Crear el comando SQL para el procedimiento almacenado
                 SqlCommand da = new SqlCommand("MostrarUltimoTema", Conexion.conectar);
                 da.CommandType = CommandType.StoredProcedure;
                 da.Parameters.AddWithValue("@IdProfesor", idProfesor);
                 da.Parameters.AddWithValue("@IdCarrera", idcarrera);
                 da.Parameters.AddWithValue("@IdAsignatura", idasignatura);
                 SqlDataReader reader = da.ExecuteReader();
+
+                //Ejecutar el comando y leer el resultado
                 if (reader.Read())
                 {
                     string tema = reader["Tema"].ToString();
@@ -38,28 +43,40 @@ namespace UNAN.Datos
             }
             finally
             {
+                //Cerrar la conexión a l abase de datos
                 Conexion.cerrar();
             }
         }
+
+        /// <summary>
+        /// Muestra los temas no impartidos por profesor desde la base de datos en un DataTable que se autocompleta con dicha información.
+        /// </summary>
+        /// <param name="dt">DataTable en el cual se mostrarán los datos</param>
+        /// <param name="parametros">Variables de filtro para los temas</param>
         public void MostrarTemasAtrasados(ref DataTable dt,LAvanceProgramatico parametros)
         {
             try
             {
+                //Abrir la conexión a la base de datos
                 Conexion.abrir();
+
+                //Cerar el comando SQL para el procedimiento almacenado
                 SqlDataAdapter da = new SqlDataAdapter("MostrarTemasAtrasados", Conexion.conectar);
                 da.SelectCommand.CommandType = CommandType.StoredProcedure;
                 da.SelectCommand.Parameters.AddWithValue("@IdProfesor", parametros.IdProfesor);
                 da.SelectCommand.Parameters.AddWithValue("@IdCarrera", parametros.IdCarrera);
                 da.SelectCommand.Parameters.AddWithValue("@IdAsignatura", parametros.IdAsignatura);
                 da.Fill(dt);
-
-
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            finally { Conexion.cerrar(); }
+            finally
+            { 
+                //Cerrar la conexión a la bade de datos
+                Conexion.cerrar(); 
+            }
         }
     }
 }
--- UNAN/Datos/DCarreras.cs
+++ UNAN/Datos/DCarreras.cs
@@ -8,13 +8,22 @@ namespace UNAN.Datos
 {
     public class DCarreras
     {
+        //variable para guardar el codigo de la carrera
 		public string cod;
 
+        /// <summary>
+        /// Inserta una nueva carrera en la base de datos.
+        /// </summary>
+        /// <param name="parametros">Un objeto de tipo LCarreras que contiene los parámetros para la inserción.</param>
+        /// <returns>Devuelve true si la inserción fue exitosa; de lo contrario, devuelve false.</returns>
         public bool InsertarCarrera(LCarreras parametros)
 		{
 			try
 			{
+                //Abrir la conexión a la base de datos
 				Conexion.abrir();
+
+                //Crear el comando SQL para el procedimiento almacenado
 				SqlCommand cmd = new SqlCommand("Insertar_Carrera", Conexion.conectar);
 				cmd.CommandType = CommandType.StoredProcedure;
 				cmd.Parameters.AddWithValue("@NombreC", parametros.NombreC);
@@ -30,29 +39,48 @@ namespace UNAN.Datos
 			}
 			finally
 			{
+                //Cerrar la conexión a la base de datos
 				Conexion.cerrar();
 			}
 		}
 
+        /// <summary>
+        /// Muestra las carreras desde la base de datos en un ComboBox que se autocompleta con dicha información.
+        /// </summary>
+        /// <param name="combo">Combobox en el cual se mostraran las carreras</param>
+        /// <param name="modalidad">Modalidad parafiltrar las carreras</param>
 		public void MostrarCarrera(ComboBox combo,string modalidad)
 		{
             try
             {
+                //Crear la colección para el autocompletado
                 AutoCompleteStringCollection lista = new AutoCompleteStringCollection();
+
+                //Abrir la conexión a la base de datos
                 Conexion.abrir();
+
+                //Crear el comando SQL para el procedimiento almacenado
                 SqlCommand da = new SqlCommand("MostrarCarrera", Conexion.conectar);
                 da.CommandType = CommandType.StoredProcedure;
                 da.Parameters.AddWithValue("@Mod", modalidad);
+
+                //Crear el adapatador para ejecuatr el comando y llenar los datos en el DataTable
                 SqlDataAdapter cb = new SqlDataAdapter(da);
                 DataTable dt = new DataTable();
                 cb.Fill(dt);
+
+                //Configurar el combobox para mostrar los datos
                 combo.ValueMember = "IdCarreras";
                 combo.DisplayMember = "NombreC";
                 combo.DataSource = dt;
+
+                //Agregar los datos a la colección de autocompletado
                 for (int i = 0; i < dt.Rows.Count; i++)
                 {
                     lista.Add(dt.Rows[i]["NombreC"].ToString());
                 }
+
+                //Asignar la colección de autocompletado al combobox
                 combo.AutoCompleteCustomSource= lista;
             }
             catch (Exception ex)
@@ -61,21 +89,31 @@ namespace UNAN.Datos
             }
             finally
             {
+                //Cerrar la conexión a la base de datos
                 Conexion.cerrar();
             }
         }
 
+        /// <summary>
+        /// Muestra el codigo de carrera desde la base de datos en un Label que se autocompleta con dicha información.
+        /// </summary>
+        /// <param name="carr">Carr para filtrar el codigo</param>
+        /// <param name="codi">codi para asignar el codigo de carrera</param>
         public void MostrarCodigoC(string carr, Label codi)
         {
             try
             {
+                //Abrir la conexión a la base de datos
                 Conexion.abrir();
+
+                //Crear el comando SQL para el procedimeinto almacenado
                 SqlDataAdapter da = new SqlDataAdapter("MostrarCodigoC", Conexion.conectar);
                 da.SelectCommand.CommandType = CommandType.StoredProcedure;
                 da.SelectCommand.Parameters.AddWithValue("@Cod", carr);
                 DataTable codigo = new DataTable();
                 da.Fill(codigo);
 
+                //ASignar el dato al label
                 if (codigo.Rows.Count > 0)
                 {
                     codi.Text = codigo.Rows[0]["CodigoC"].ToString();
@@ -91,6 +129,7 @@ namespace UNAN.Datos
             }
             finally
             {
+                //Cerrar la conexión a la base de datos
                 Conexion.cerrar();
             }
         }
